5e122a63cfb41813526b8938beecbab290d2360b
TomohiroShibata <tomohiro_shibata@yanmar.com>
2021-06-22 08:35:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ def setup_package():
             "tornado",
             "twisted",
             "urllib3",
+            "lightgbm",
         ],
         entry_points={
             "console_scripts": [
